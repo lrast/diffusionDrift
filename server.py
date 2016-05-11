0e1c2a038d10ffe39a765a6512655e9e5bf7b4ef
luke <luke.rast@gmail.com>
2016-05-11 02:28:37 -0400

    working

--- server.py
+++ server.py
@@ -16,6 +16,30 @@ def serverTest():
     #testing and debug page
     return render_template("working.html")
 
+@app.route('/lesson1')
+def lesson1():
+    #home page
+    return render_template("lesson1.html")
+
+@app.route('/lesson2')
+def lesson2():
+    #home page
+    return render_template("lesson2.html")
+
+@app.route('/lesson3')
+def lesson3():
+    #home page
+    return render_template("lesson3.html")
+
+@app.route('/lesson4')
+def lesson4():
+    #home page
+    return render_template("lesson4.html")
+
+@app.route('/lesson5')
+def lesson5():
+    #home page
+    return render_template("lesson5.html")
 
 if __name__ == '__main__':
     app.run()
